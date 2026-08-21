44aa9c2512587b235bc7e99435e08dfc6e44ff94
maychu1411 <145586014+maychu1411@users.noreply.github.com>
2026-08-21 15:49:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Dán URL Web App /exec sau khi Deploy Apps Script.
+window.LMS_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbyixNdxRTTXX8BHjevPTnWAy3ZhXLv80OplEcoVsztVVBmOoxvOsD7-E6psSCkXEDFR/exec',
+  APP_NAME: 'QUẢN LÝ LỚP',
+  SCHOOL_NAME: '',
+  CACHE_VERSION: 'v1'
+};
